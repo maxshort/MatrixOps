8131feb46e793bef8eebc08ec6900c70acc60c12
maxshort <maxshort@users.noreply.github.com>
2014-09-12 19:42:49 -0500

    Equivalence in both.
    Went with mathematically equivalent to 8/4=4/2=2

--- matrix.py
+++ matrix.py
@@ -2,6 +2,10 @@ import types
 import collections
 
 #all operations in this class might not be reduced to their lowest possible value
+#most methods return a new Fraction but _reduce alters values. _reduce is only
+#for internal class use.
+#Also don't use num and denom except as getters (might make these _ prefixed in
+#future.
 class Fraction(object):
     #creates a new fraction, denom cannot be 0
     def __init__(self,numerator, denominator=1):
@@ -26,6 +30,40 @@ class Fraction(object):
         return Fraction(self.num*other.denom,other.num*self.denom)
     def __str__(self):
         return str(self.num)+"\n-----\n"+str(self.denom)
+    #uses euler's method to find the gcd of two numbers
+    @staticmethod
+    def gcd(num1, num2):
+        if (num1==0):
+            return num2
+        if (num2==0):
+            return num1
+        newNum = num1%num2
+        return Fraction.gcd(num2,newNum)
+    def copy(self):
+        return Fraction(self.num,self.denom)
+    def reduce(self):
+        eGcd = Fraction.gcd(self.num,self.denom)
+        self.num /=eGcd
+        self.denom /=eGcd
+    #comparison should have two integer arguments -- intended for <(_lt_) etc.
+    def __compare(self,other,comparison):
+        commonDenom = self.denom*other.denom
+        newNumSelf = self.num*other.denom
+        newNumOther = other.num*self.denom
+        return comparison(newNumSelf,newNumOther)
+    #less than
+    def __lt__(self,other):
+        return self.__compare(other,lambda num1,num2:num1<num2)
+    def __le__(self,other):
+        return self.__compare(other,lambda num1,num2:num1<=num2)
+    def __eq__(self,other):
+        return self.__compare(other,lambda num1,num2:num1==num2)
+    def __ne__(self,other):
+        return self.__compare(other,lambda num1,num2:num1!=num2)
+    def __gt__(self,other):
+        return self.__compare(other,lambda num1,num2:num1>num2)
+    def __ge__(self,other):
+        return self.__compare(other,lambda num1,num2:num1>=num2)
 #returns the most reduced fraction from all operations
 #Add/subtract/mult/div are weird b/c they trying to make immutable.
     #might change that.
@@ -49,20 +87,10 @@ class AutoReduceFraction(Fraction):
         answer = super().divideBy(other)
         a = AutoReduceFraction(answer.num,answer.denom)
         return a
-    #uses euler's method to find the gcd of two numbers
-    @staticmethod
-    def gcd(num1, num2):
-        if (num1==0):
-            return num2
-        if (num2==0):
-            return num1
-        newNum = num1%num2
-        return AutoReduceFraction.gcd(num2,newNum)
-    def reduce(self):
-        eGcd = AutoReduceFraction.gcd(self.num,self.denom)
-        self.num /=eGcd
-        self.denom /=eGcd
-        #########################LEFT OFF HERE...
+    def copy(self):
+        return AutoReduceFraction(self.num,self.denom)
+    
+        #LEFT OFF HERE...
 class RowOperations:
     def multiplyBy(rowList,value):
         ret = []
@@ -102,8 +130,8 @@ class Matrix:
 #Test for accepting 1d matrix
 ##x = Matrix([1,2,3])
 ##x.printMatrix()
-
-#Test for accepting 3d matrix
+##
+###Test for accepting 3d matrix
 ##x = Matrix([[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]])
 ##x.printMatrix()
 
@@ -120,104 +148,104 @@ class Matrix:
 ##f3 = f1.add(f2)
 ##print(f3.num)
 ##print(f3.denom)
-
-#Two Whole numbers
+##
+###Two Whole numbers
 ##f1 = Fraction(8)
 ##f2 = Fraction(10)
 ##f3 = f2.add(f1)
 ##print (f3.num)
 ##print(f3.denom)
-
-#Whole number and fraction
+##
+###Whole number and fraction
 ##f1 = Fraction(7)
 ##f2 = Fraction(3,2)
 ##f3 = f2.add(f1)
 ##print(f3.num)
 ##print(f3.denom)
-
-#Two fractions
+##
+###Two fractions
 ##f1 = Fraction(3,2)
 ##f2 = Fraction(2,3)
 ##f3 = f2.add(f1)
 ##print (f3.num)
 ##print (f3.denom)
-
-#Negative and positive fraction
+##
+###Negative and positive fraction
 ##f1 = Fraction(-2,3)
 ##f2 = Fraction(3,8)
 ##f3 = f1.add(f2)
 ##print(f3.num)
 ##print(f3.denom)
-
-#Subtracting whole number
+##
+###Subtracting whole number
 ##f1 = Fraction(2)
 ##f2 = Fraction(1)
 ##f3 = f1.subtract(f2)
 ##print(f3.num)
 ##print (f3.denom)
-
-#Subtracting fractions
+##
+###Subtracting fractions
 ##f1 = Fraction(2,3)
 ##f2 = Fraction(3,8)
 ##f3 = f1.subtract(f2)
 ##print(f3.num)
 ##print (f3.denom)
-
-#Multiplying whole numbers
+##
+###Multiplying whole numbers
 ##f1 = Fraction(3)
 ##f2 = Fraction(8)
 ##f3 = f1.multiply(f2)
 ##print(f3.num)
 ##print (f3.denom)
-
-#Multiplying Fractions
+##
+###Multiplying Fractions
 ##f1 = Fraction(3,4)
 ##f2 = Fraction(7,8)
 ##f3 = f1.multiply(f2)
 ##print(f3.num)
 ##print (f3.denom)
-
-#Dividing Whole Number that divide evenly
+##
+###Dividing Whole Number that divide evenly
 ##f1 = Fraction(12)
 ##f2 = Fraction(3)
 ##f3 = f1.divideBy(f2)
 ##print (f3.num)
 ##print(f3.denom)
-
-#Dividing Whole number that do not divide evenly
+##
+###Dividing Whole number that do not divide evenly
 ##f1 = Fraction(15)
 ##f2 = Fraction(7)
 ##f3 = f1.divideBy(f2)
 ##print (f3.num)
 ##print (f3.denom)
-
-#Testing the autoreduce fraction inheritance
+##
+###Testing the autoreduce fraction inheritance
 ##f1 = AutoReduceFraction(2)
 ##f2 = AutoReduceFraction(4)
 ##f3 = f1.add(f2)
 ##print (f3.num)
 ##print (f3.denom)
-
-#Test of static method
-##print (AutoReduceFraction.gcd(4,8))
-
-#test of whole numbers
+##
+###Test of static method
+##print (Fraction.gcd(4,8))
+##
+###test of whole numbers
 ##arf1 = AutoReduceFraction(2)
 ##arf2 = AutoReduceFraction(3,1)
 ##arf3 = arf1.add(arf2)
 ##print(arf3.num)
 ##print(arf3.denom)
-
+##
 ##arf1 = AutoReduceFraction(3,2)
 ##arf3 = arf1.add(arf1)
 ##print (arf3)
-
-#testing the subtraction of whole numbers
+##
+###testing the subtraction of whole numbers
 ##arf1 = AutoReduceFraction(4)
 ##arf2 = AutoReduceFraction(2)
 ##print (arf1.subtract(arf2))
-
-#subtraction of fraction
+##
+###subtraction of fraction
 ##arf1 = AutoReduceFraction(18,27)
 ##arf2 = AutoReduceFraction(9,27)
 ##print(arf1.subtract(arf2))
@@ -226,28 +254,50 @@ class Matrix:
 ##arf1 = AutoReduceFraction(7,2)
 ##arf2 = AutoReduceFraction(1,2)
 ##print(arf1.subtract(arf2))
-
-#multiplication of whole numbers
+##
+###multiplication of whole numbers
 ##arf1 = AutoReduceFraction(5)
 ##arf2 = AutoReduceFraction(3)
 ##print (arf1.multiply(arf2))
-
-#multiplication of a whole number and fraction
+##
+###multiplication of a whole number and fraction
 ##arf1 = AutoReduceFraction(8)
 ##arf2 = AutoReduceFraction(1,2)
 ##print(arf1.multiply(arf2))
-
-#divding divisible numbers
+##
+###divding divisible numbers
 ##arf1 = AutoReduceFraction(8)
 ##arf2 = AutoReduceFraction(4)
 ##print (arf1.divideBy(arf2))
-#dividing non-divisible whole numbers
+###dividing non-divisible whole numbers
 ##arf1 = AutoReduceFraction(18)
 ##arf2 = AutoReduceFraction(4)
 ##print(arf1.divideBy(arf2))
+##
+###dividing fractions that result in fraction
+##arf1 = AutoReduceFraction(9,2)
+##arf2 = AutoReduceFraction(7,3)
+##print(arf1.divideBy(arf2))
 
-#dividing fractions that result in fraction
-arf1 = AutoReduceFraction(9,2)
-arf2 = AutoReduceFraction(7,3)
-print(arf1.divideBy(arf2))
+#testing signs
 
+##arf1 = AutoReduceFraction(3,2)
+##arf2 = AutoReduceFraction(7,8)
+##print(arf1<arf2)
+##print(arf2<arf1)
+##print(arf1<=arf2)
+##print(arf2<=arf1)
+##print(arf1>arf2)
+##print (arf2>=arf1)
+##print(arf1==arf2)
+##print(arf1!=arf2)
+
+#Testing equality with equal fractions
+##arf1 = AutoReduceFraction(18,4)
+##arf2 = AutoReduceFraction(9,2)
+##print(arf1==arf2)
+##print(arf1!=arf2)
+
+arf1 = AutoReduceFraction(8,4)
+arf2 = AutoReduceFraction(4,2)
+print (arf1==arf2)
